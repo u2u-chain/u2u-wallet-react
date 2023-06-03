f7056225f63b0fbfef7636ab5e4a15728a0f97a5
Edward Hedenberg <edward@uniultra.xyz>
2023-06-03 22:12:11 +0700

    :sparkles: price formatter

--- src/views/app-views/Wallet/index.tsx
+++ src/views/app-views/Wallet/index.tsx
@@ -22,10 +22,6 @@ export default function WalletPage() {
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: currencyCode,
-
-    // These options are needed to round to whole numbers if that's what you want.
-    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
   });
   const calculatedPrice = formatter.format(balance * currencyRate);
 
