0bc17e1c0cd7b0c5ec6c026a08f75fa90fc77b35
Edward Hedenberg <edward@uniultra.xyz>
2023-06-04 23:23:14 +0700

    :zap: responsive hero area

--- src/views/app-views/Landing/LandingHero.tsx
+++ src/views/app-views/Landing/LandingHero.tsx
@@ -8,19 +8,21 @@ export default function LandingHero() {
   return <div className={styles.hero}>
     <Row gutter={[24, 24]}>
       <Col xs={24} sm={24} md={12}>
-        <div className={styles.heroTitlePromo}>
-          <Typography.Text className={styles.heroTitle}>
-            Supercharge your Web3 Experience
-          </Typography.Text>
-          <Typography.Text className={styles.heroDescription}>
-            Manage all your U2U Tokens, exchange NFTs and explore many applications
-          </Typography.Text>
+        <div className={styles.heroContent}>
+          <div className={styles.heroTitlePromo}>
+            <Typography.Text className={styles.heroTitle}>
+              Supercharge your Web3 Experience
+            </Typography.Text>
+            <Typography.Text className={styles.heroDescription}>
+              Manage all your U2U Tokens, exchange NFTs and explore many applications
+            </Typography.Text>
+          </div>
+          <Link to={'/wallet'}>
+            <Button shape={'round'} type={'primary'} size={'large'}>
+              Access Your Wallet <ArrowRightOutlined/>
+            </Button>
+          </Link>
         </div>
-        <Link to={'/wallet'}>
-          <Button shape={'round'} type={'primary'} size={'large'}>
-            Access Your Wallet <ArrowRightOutlined/>
-          </Button>
-        </Link>
       </Col>
       <Col xs={24} sm={24} md={12}>
         <div className={styles.figure}>
