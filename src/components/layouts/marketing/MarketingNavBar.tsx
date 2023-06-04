a9838c779b0db06f80a3b4bde685c61535877098
Edward Hedenberg <edward@uniultra.xyz>
2023-06-04 23:48:05 +0700

    :zap: responsive marketing menu

--- src/components/layouts/marketing/MarketingNavBar.tsx
+++ src/components/layouts/marketing/MarketingNavBar.tsx
@@ -1,14 +1,87 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./marketing.module.css";
-import {Button, Dropdown, Space, Typography} from "antd";
+import {Button, Divider, Drawer, Dropdown, List, Menu, Space, theme, Typography} from "antd";
 import Container from "@/components/common/Container.tsx";
+import {HomeOutlined, InfoOutlined, MenuOutlined, QuestionOutlined, WalletOutlined} from "@ant-design/icons";
+import {Link, useNavigate} from "react-router-dom";
 
 interface MarketingNavBarProps {
 
 }
 
 export default function MarketingNavBar(props: MarketingNavBarProps) {
+  const [drawerOpened, setDrawerOpened] = useState(false);
+  const {
+    token: {
+      colorBgMask
+    }
+  } = theme.useToken();
+  const navigate = useNavigate();
+
   return <div className={styles.marketingNavbar}>
+
+    <Drawer
+      headerStyle={{display: 'none'}}
+      placement="right" open={drawerOpened}
+      onClose={() => setDrawerOpened(false)}
+      bodyStyle={{padding: 0, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}
+    >
+      <div>
+        <div style={{padding: '24px 0 0 24px'}}>
+          <Typography.Text className={styles.logoText}>
+            U2U Wallet
+          </Typography.Text>
+        </div>
+        <Divider/>
+        <Menu
+          selectable={false}
+          style={{backgroundColor: 'transparent', borderInlineEnd: 'none'}}
+          items={[{
+            key: 'home',
+            label: 'Home',
+            icon: <HomeOutlined/>,
+            onClick: () => {
+              setDrawerOpened(false);
+              navigate('/');
+            }
+          }, {
+            key: 'about',
+            label: 'About',
+            icon: <InfoOutlined/>,
+            onClick: () => {
+              setDrawerOpened(false);
+              navigate('/about');
+            }
+          }, {
+            key: 'faqs',
+            label: 'FAQs',
+            icon: <QuestionOutlined/>,
+            onClick: () => {
+              setDrawerOpened(false);
+              navigate('/faqs');
+            }
+          }]}
+        />
+      </div>
+      <Space
+        style={{
+          padding: '24px',
+          width: '100%',
+        }}
+        direction={'vertical'}
+      >
+        <Link to={'/wallet'}>
+          <Button block size={'large'}>
+            Access
+          </Button>
+        </Link>
+        <Link to={'/auth/register'}>
+          <Button block size={'large'} type={'primary'}>
+            Create Wallet
+          </Button>
+        </Link>
+      </Space>
+    </Drawer>
     <Container>
       <div className={styles.marketingNavbarInner}>
         <div className={styles.logo}>
@@ -24,7 +97,7 @@ export default function MarketingNavBar(props: MarketingNavBarProps) {
               key: 1,
               label: 'Mainnet'
             }]
-          }} trigger={['click']}>
+          }} trigger={['click']} className={styles.networkSelector}>
             <Button type={'text'} size={'small'}>
               Test net
             </Button>
@@ -52,6 +125,16 @@ export default function MarketingNavBar(props: MarketingNavBarProps) {
             </Button>
             <Button shape={'round'} href={'/auth/register'}>
               Create Wallet
+            </Button>
+          </Space>
+        </div>
+        <div className={styles.mobileActions}>
+          <Space>
+            <Button type={'text'} href={'/auth/login'}>
+              <WalletOutlined/>
+            </Button>
+            <Button type={'text'} onClick={() => setDrawerOpened(true)}>
+              <MenuOutlined/>
             </Button>
           </Space>
         </div>
