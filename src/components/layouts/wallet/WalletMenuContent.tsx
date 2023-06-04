13a977ccee446805c4dc6ca037cf6adfd6f53842
Edward Hedenberg <edward@uniultra.xyz>
2023-06-04 17:17:03 +0700

    :sparkles: navigation icons

--- src/components/layouts/wallet/WalletMenuContent.tsx
+++ src/components/layouts/wallet/WalletMenuContent.tsx
@@ -1,27 +1,33 @@
 import React from "react";
 import {Menu, theme} from "antd";
 import {useLocation, useNavigate} from "react-router-dom";
+import {HistoryOutlined, HomeOutlined, KeyOutlined, ToolOutlined, WalletOutlined} from "@ant-design/icons";
 
 const routes = [{
   url: '/wallet',
   key: '/wallet',
-  label: 'Home'
+  label: 'Home',
+  icon: <HomeOutlined/>,
 }, {
   url: '/wallet/assets/tokens',
   key: '/wallet/assets/tokens',
-  label: 'Assets'
+  label: 'Assets',
+  icon: <WalletOutlined/>,
 }, {
   url: '/wallet/history',
   key: '/wallet/history',
-  label: 'History'
+  label: 'History',
+  icon: <HistoryOutlined/>,
 }, {
   url: '/wallet/keys',
   key: '/wallet/keys',
-  label: 'Keys'
+  label: 'Keys',
+  icon: <KeyOutlined/>,
 }, {
   url: '/wallet/tools',
   key: '/wallet/tools',
-  label: 'Tools'
+  label: 'Tools',
+  icon: <ToolOutlined/>,
 }]
 
 export const WalletMenuContent = () => {
@@ -40,6 +46,7 @@ export const WalletMenuContent = () => {
       items={routes.map(route => ({
         key: route.key,
         label: route.label,
+        icon: route.icon,
         onClick: () => navigate(route.url)
       }))}
     />
