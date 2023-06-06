47e912db1f250b73b93db562b0ad41ee5de94bcf
Edward Hedenberg <edward@uniultra.xyz>
2023-06-07 04:29:00 +0700

    :lipstick: change sign in method selector

--- src/views/auth-views/Login/index.tsx
+++ src/views/auth-views/Login/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Tabs, Typography} from "antd";
+import React, {useState} from "react";
+import {Button, Divider, Space, Tabs, Typography} from "antd";
 import styles from './login.module.css';
 import LogoGradient from "@/assets/logo_gradient.png";
 import LoginForm from "@/components/auth/LoginForm.tsx";
@@ -7,11 +7,11 @@ import LoginWithKeyPair from "@/components/auth/LoginWithKeyPair.tsx";
 import LoginWithKeystore from "@/components/auth/LoginWithKeystore.tsx";
 import LoginWithMnemonic from "@/components/auth/LoginWithMnemonic.tsx";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBrain, faFileLock, faKey, faUser} from "@fortawesome/pro-solid-svg-icons";
+import {faBrain, faFileLock, faKey, faLock, faUser} from "@fortawesome/pro-solid-svg-icons";
 import {Link} from "react-router-dom";
 
 export default function LoginPage() {
-
+  const [loginMethod, setLoginMethod] = useState('password');
   return (
     <div style={{width: '100%'}}>
       <img src={LogoGradient} alt={'u2u wallet'} className={styles.logo}/>
@@ -22,36 +22,54 @@ export default function LoginPage() {
         Sign in to access your U2U Wallet & digital assets...
       </Typography.Paragraph>
       <div>
-        <Tabs
-          items={[{
-            key: 'password',
-            label: <>
-              <FontAwesomeIcon icon={faUser}/> Account
-            </>,
-            children: <LoginForm/>
-          }, {
-            key: 'key-pairs',
-            label: <>
-              <FontAwesomeIcon icon={faKey}/> Private Key
-            </>,
-            children: <LoginWithKeyPair/>
-          }, {
-            key: 'mnemonic',
-            label: <>
-              <FontAwesomeIcon icon={faBrain}/> Mnemonic Phrase
-            </>,
-            children: <LoginWithMnemonic/>
-          }, {
-            key: 'keystore',
-            label: <>
-              <FontAwesomeIcon icon={faFileLock}/> Keystore
-            </>,
-            children: <LoginWithKeystore/>
-          }]}
-        />
+        {loginMethod === 'password' && <LoginForm/>}
+        {loginMethod === 'private-key' && <LoginWithKeyPair/>}
+        {loginMethod === 'mnemonic' && <LoginWithMnemonic/>}
+        {loginMethod === 'keystore' && <LoginWithKeystore/>}
         <Typography.Paragraph style={{marginTop: 10, color: 'var(--text-light)'}}>
           Haven't got an account yet? <Link to={'/auth/register'}>Create One</Link>
         </Typography.Paragraph>
+        <Divider style={{marginTop: 64}}>
+          Other login methods
+        </Divider>
+        <Space direction={'vertical'} style={{width: '100%'}}>
+          {loginMethod !== 'password' && (
+            <Button
+              block
+              icon={<FontAwesomeIcon icon={faLock}/>}
+              onClick={() => setLoginMethod('password')}
+            >
+              Password
+            </Button>
+          )}
+          {loginMethod !== 'private-key' && (
+            <Button
+              block
+              icon={<FontAwesomeIcon icon={faKey}/>}
+              onClick={() => setLoginMethod('private-key')}
+            >
+              Private Key
+            </Button>
+          )}
+          {loginMethod !== 'mnemonic' && (
+            <Button
+              block
+              icon={<FontAwesomeIcon icon={faBrain}/>}
+              onClick={() => setLoginMethod('mnemonic')}
+            >
+              Mnemonic Phrase
+            </Button>
+          )}
+          {loginMethod !== 'keystore' && (
+            <Button
+              block
+              icon={<FontAwesomeIcon icon={faFileLock}/>}
+              onClick={() => setLoginMethod('keystore')}
+            >
+              Keystore
+            </Button>
+          )}
+        </Space>
       </div>
     </div>
   )
