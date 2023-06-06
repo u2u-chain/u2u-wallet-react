590af0705e27ac65d361af1feaac2ddb56baebdd
Edward Hedenberg <edward@uniultra.xyz>
2023-06-07 00:15:19 +0700

    :lipstick: auth ui with figure

--- src/components/layouts/auth/AuthLayout.tsx
+++ src/components/layouts/auth/AuthLayout.tsx
@@ -2,8 +2,10 @@ import React, {useEffect} from "react";
 import styles from "./auth.module.css";
 import {Outlet, useNavigate} from "react-router-dom";
 import {useAppSelector} from "@/redux/store.ts";
+import BgConnect from "@/assets/images/bg-connect.svg";
 
 import LogoWhite from "@/assets/logo_white.png";
+import {Typography} from "antd";
 
 export default function AuthLayout() {
   const {isLoggedIn} = useAppSelector(state => state.auth);
@@ -16,8 +18,17 @@ export default function AuthLayout() {
 
   return (
     <div className={styles.authOuter}>
-      <div className={styles.authFigure}>
-
+      <div className={styles.authFigure} style={{
+        backgroundImage: `url(${BgConnect})`
+      }}>
+        <div className={styles.authFigureContent}>
+          <Typography.Title>
+            Send & Receive U2U
+          </Typography.Title>
+          <Typography.Paragraph>
+            All your assets in your pocket, decentralized, secure.
+          </Typography.Paragraph>
+        </div>
       </div>
       <div className={styles.authSection}>
         <div className={styles.logoWrapper}>
